7d5185c7bddaf3dc69d9f28c89e8bfd521384329
swenger <spwenger@googlemail.com>
2011-08-22 12:11:31 +0200

    cleaned up for pyglet contribution

--- audioplayer.py
+++ audioplayer.py
@@ -1,22 +1,31 @@
 import pyglet
 
 class ArraySource(pyglet.media.StaticMemorySource):
+    '''A source that has been created from a numpy array.'''
+
     def __init__(self, rate, data):
-        """Create a pyglet audio source from a numpy array.
+        '''Construct an `ArraySource` for the data in `data`.
         
-        ``rate`` is the sampling rate in Hertz.
-        ``data`` is a c-contiguous numpy array of dimension (num_samples, num_channels)."""
+        :Parameters:
+            `rate` : `int`
+                The sampling rate in Hertz.
+            `data` : `array`
+                A c-contiguous numpy array of dimension (num_samples, num_channels).
+        '''
         
-        if data.ndim not in (1, 2) or not data.flags.c_contiguous:
-            raise ValueError("data must be a c-contiguous array of shape (num_samples, num_channels).""")
+        if data.ndim not in (1, 2):
+            raise ValueError("The data array must be one- or two-dimensional.""")
+
+        if not data.flags.c_contiguous:
+            raise ValueError("The data array must be c-contiguous.""")
 
         num_channels = data.shape[1] if data.ndim > 1 else 1
         if num_channels not in [1, 2]:
-            raise ValueError("only mono and stereo audio are supported.""")
+            raise ValueError("Only mono and stereo audio are supported.""")
         
         num_bits = data.dtype.itemsize * 8
         if num_bits not in [8, 16]:
-            raise ValueError("only 8 and 16 bit audio are supported.""")
+            raise ValueError("Only 8 and 16 bit audio are supported.""")
         
         audio_format = pyglet.media.AudioFormat(num_channels, num_bits, rate)
 
@@ -28,18 +37,19 @@ class ArraySource(pyglet.media.StaticMemorySource):
 
 
 if __name__ == "__main__":
+    import sys
     from scipy.io import wavfile
-    infilename = "Crag Lake.wav"
-    sound = ArraySource(*wavfile.read(infilename)) # sound = pyglet.media.load(infilename)
-    print sound.audio_format, sound.duration
 
+    sound = ArraySource(*wavfile.read(sys.argv[1]))
+
+    """
     window = pyglet.window.Window()
 
     @window.event
     def on_draw():
         window.clear()
-        # TODO draw a coordinate system and a live plot of playback
         pyglet.graphics.draw(2, pyglet.gl.GL_LINES, ("v2f", (10, 10, 10, 10 + player.time)))
+        """
 
     player = pyglet.media.Player()
     player.queue(sound)
