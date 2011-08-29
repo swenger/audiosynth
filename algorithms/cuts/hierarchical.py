7afa8a1f73901b36cdf471ee34055932b394829e
swenger <spwenger@googlemail.com>
2011-08-29 15:51:02 +0200

    removed skip_diagonal option, replaced by TODO for min cut length

--- algorithms/cuts/hierarchical.py
+++ algorithms/cuts/hierarchical.py
@@ -1,13 +1,13 @@
 import heapq
 
-from numpy import isnan, inf, eye, unravel_index, asarray, isinf, floor, log
+from numpy import inf, eye, unravel_index, asarray, isinf, floor, log
 from scipy.spatial.distance import cdist
 from numpy.fft import fft
 
 from ..algorithm import CutsAlgorithm, Cut
 
 class AnalysisLayer(object):
-    def __init__(self, data1, data2, block_length, num_keep, block_length_shrink=16, num_skip_print=3, skip_diagonal=False):
+    def __init__(self, data1, data2, block_length, num_keep, block_length_shrink=16, num_skip_print=3):
         self.block_length = block_length
         if block_length >= block_length_shrink ** num_skip_print: # do not print innermost num_skip_print layers
             print "len(data1) = %d, len(data2) = %d, block_length = %d, num_keep = %d" % (len(data1), len(data2), block_length, num_keep)
@@ -32,9 +32,9 @@ class AnalysisLayer(object):
         # compute distances between feature vectors
         distances = cdist(feature_vectors1, feature_vectors2, "sqeuclidean") # (u - v) ** 2
         normalization = cdist(feature_vectors1, -feature_vectors2, "sqeuclidean") # (u + v) ** 2
-        distances /= normalization # (u - v) ** 2 / (u + v) ** 2
-        distances[isnan(distances)] = 0.0 # both feature vectors are zero => cut okay
-        if skip_diagonal and data1 is data2: # data arrays are identical => do not cut on diagonal (equivalent to a jump to the same location)
+        distances[normalization != 0] /= normalization[normalization != 0] # (u - v) ** 2 / (u + v) ** 2
+        distances[normalization == 0] = 0.0 # both feature vectors are zero => cut okay
+        if data1 is data2: # disallow very short cuts TODO use minimal cut length instead
             distances[eye(len(distances)).astype(bool)] = inf
 
         # find best num_keep off-diagonal child indices and their respective distances
@@ -53,7 +53,7 @@ class AnalysisLayer(object):
             for i, j, d in zip(self.i, self.j, self.d):
                 if not isinf(d):
                     self.children.append(AnalysisLayer(blocks1[i], blocks2[j], new_block_length, new_num_keep,
-                        block_length_shrink, num_skip_print, skip_diagonal))
+                        block_length_shrink, num_skip_print))
 
     def get_cuts(self, weight_factor=2.0, weight=1.0):
         """Return a list of all branches of the tree with their respective weighted length."""
@@ -70,13 +70,12 @@ class AnalysisLayer(object):
 class HierarchicalCutsAlgorithm(CutsAlgorithm):
     """Hierarchical algorithm for finding cuts."""
     
-    def __init__(self, num_cuts=256, num_keep=40, block_length_shrink=16, num_levels="max", weight_factor=1.2, skip_diagonal=False):
+    def __init__(self, num_cuts=256, num_keep=40, block_length_shrink=16, num_levels="max", weight_factor=1.2):
         self.num_cuts = int(num_cuts)
         self.num_keep = int(num_keep)
         self.block_length_shrink = int(block_length_shrink)
         self.num_levels = num_levels if num_levels == "max" else int(num_levels)
         self.weight_factor = float(weight_factor)
-        self.skip_diagonal = bool(skip_diagonal)
 
     def __call__(self, data):
         num_levels = min(int(floor(log(0.5 * len(data)) / log(self.block_length_shrink))) + 1,
@@ -89,7 +88,7 @@ class HierarchicalCutsAlgorithm(CutsAlgorithm):
         
         block_length = self.block_length_shrink ** (num_levels - 1)
         data = data[:block_length * (len(data) // block_length)]
-        root = AnalysisLayer(data, data, block_length, self.num_cuts, self.block_length_shrink, 3, self.skip_diagonal)
+        root = AnalysisLayer(data, data, block_length, self.num_cuts, self.block_length_shrink, 3)
         cuts = root.get_cuts(self.weight_factor)
         cuts.sort(key=lambda x: x[2])
 
