5c71c1acf78f23db2ed95437dea7db9a7fcbd859
swenger <spwenger@googlemail.com>
2011-09-02 11:27:26 +0200

    added distance matrices plot

---
+++ distance_matrices_plot.py
@@ -0,0 +1,28 @@
+import sys
+
+from numpy import load, isfinite
+from pylab import figure, axes, draw, show
+
+d = load(sys.argv[1]).item() # .npy file generated using the distance_matrices_filename option of HierarchicalCutsPlot
+
+levels = sorted(set(x[1] - x[0] for x in d))
+
+for l in reversed(levels[-3:]):
+    f = figure()
+    a = f.add_axes(axes())
+    a.hold(True)
+
+    vmin = min(m.min() for (left, right, bottom, top), m in d.items() if right - left == l)
+    vmax = max(m[isfinite(m)].max() for (left, right, bottom, top), m in d.items() if right - left == l)
+    
+    for (left, right, bottom, top), m in d.items():
+        if right - left == l:
+            a.imshow(m, origin="lower", extent=(left, right, bottom, top), vmin=vmin, vmax=vmax)
+    a.set_xlim(min(x[0] for x in d), max(x[1] for x in d))
+    a.set_ylim(min(x[2] for x in d), max(x[3] for x in d))
+    a.set_xticks([left for (left, right, bottom, top), m in d.items() if right - left == levels[-2]])
+    a.set_yticks([bottom for (left, right, bottom, top), m in d.items() if right - left == levels[-2]])
+
+draw()
+show()
+
